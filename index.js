449950f2968ed35c49110282fd385c5c74d066c5
tanveerkhan123456789 <tanveer.khan45450@gmail.com>
2024-07-10 07:32:37 +0500

    first commits

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const puppeteer = require('puppeteer');
 
 const client = new Client({
     puppeteer: {
-        headless: false,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--unhandled-rejections=strict',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process',
-            '--disable-gpu'
-        ],
+        headless: false, // Set to true if you want to run in headless mode
+        executablePath: puppeteer.executablePath(), // Ensure Chrome is used
     },
-    webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2410.1.html',
-    }
+    authStrategy: new LocalAuth() // Optional: Use LocalAuth to save session
 });
 
-client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
 });
 
 client.on('ready', () => {
